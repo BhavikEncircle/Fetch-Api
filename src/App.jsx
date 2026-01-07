1e213a45dc5e0603fd0192d783b65fd819bc69e9
BhavikEncircle <bhavikp.encircle@gmail.com>
2026-01-07 16:32:31 +0530

    Initialize Fetch API

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import "./App.css";
+import FetchedData from "./FetchedData";
+function App() {
+  const [fetchData, setFetchData] = useState(false);
+  console.log(fetchData);
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center ">
+        <h1 className="text-3xl font-bold">Fetch API</h1>
+        <button
+          onClick={() => setFetchData(true)}
+          className="bg-blue-500 text-white px-4 py-2 rounded-md"
+        >
+          Fetch Data
+        </button>
+        <FetchedData fetchData={fetchData} />
+        
+      </div>
+    </>
+  );
+}
+
+export default App;
